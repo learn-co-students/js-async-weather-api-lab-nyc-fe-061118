26337f3881148555efdb8ce7310ece9bc6a94110
Christian Georgeou <cgeorgeou@hotmail.com>
2018-08-23 01:06:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const API_KEY = "91e263778af92361ac356d09d927d749"
+const API_KEY = "91e263778af92361ac356d09d927d749";
 
 function handleFormSubmit(event) {
   //handle submit event
-  event.preventDefault()
+  event.preventDefault();
   let city = $(`#city`).val();
   fetchCurrentWeather(city);
-  fetchFiveDayForecast(city)
+  fetchFiveDayForecast(city);
   fetchCreateChart(city);
   }
 
@@ -13,29 +13,29 @@ function fetchCurrentWeather(city) {
   //fetch current weather based on city
     fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`)
     .then(res => res.json())
-    .then(displayCurrentWeather)
+    .then(displayCurrentWeather);
 }
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
-  let temp = json.main.temp;
-  let tempRow = document.getElementById('temp')
-  tempRow.innerHTML = temp
+  let temp = json.main.temp + "°";
+  let tempRow = document.getElementById('temp');
+  tempRow.innerHTML = temp;
   
-  let temp_min = json.main.temp_min;
-  let temp_minRow = document.getElementById("low")
+  let temp_min = json.main.temp_min + "°";
+  let temp_minRow = document.getElementById("low");
   temp_minRow.innerHTML = temp_min;
   
-  let temp_max = json.main.temp_max;
-  let temp_maxRow = document.getElementById("high")
+  let temp_max = json.main.temp_max + "°";
+  let temp_maxRow = document.getElementById("high");
   temp_maxRow.innerHTML = temp_max;
   
-  let temp_humidity = json.main.humidity;
-  let temp_humidityRow = document.getElementById("humidity")
+  let temp_humidity = json.main.humidity + "%";
+  let temp_humidityRow = document.getElementById("humidity");
   temp_humidityRow.innerHTML = temp_humidity;
   
   let temp_clouds = json.clouds.all;
-  let temp_cloudsRow = document.getElementById("cloudCover")
+  let temp_cloudsRow = document.getElementById("cloudCover");
   temp_cloudsRow.innerHTML = temp_clouds;
   
   }
@@ -44,24 +44,23 @@ function fetchFiveDayForecast(city) {
   //fetch five day forecast data based on city
     fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}&units=imperial`)
     .then(res => res.json())
-    .then(displayFiveDayForecast)
+    .then(displayFiveDayForecast);
 }
 
 function displayFiveDayForecast(forcastJson) {
   //render five day forecast data to the DOM using provided IDs and json from API
    forcastJson.list.forEach(function(dateTime) {
-      $(`#fiveDayDateTime`).append(`<div>${dateTime.dt_txt}, ${dateTime.main.humidity}, ${dateTime.main.temp}</div>`)
-    })
+      $(`#fiveDayDateTime`).append(`<div>${dateTime.dt_txt}, ${dateTime.main.humidity}, ${dateTime.main.temp}</div>`);
+    });
 }
 
 function fetchCreateChart(city) {
     fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}&units=imperial`)
     .then(res => res.json())
-    .then(createChart)
+    .then(createChart);
 }
 
 function createChart(forcastJson) {
-  debugger;
   const labels = forcastJson.list.map(function(e){
     return e.dt_txt;
   });
@@ -90,5 +89,5 @@ function createChart(forcastJson) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  $("form").on("submit", handleFormSubmit)
-})
+  $("form").on("submit", handleFormSubmit);
+});
